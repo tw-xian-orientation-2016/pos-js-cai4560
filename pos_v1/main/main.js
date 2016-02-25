1f22d1ce8cb0a8bdf1992c591ee33bcd16e70f60
cai4560 <495781086@qq.com>
2016-02-25 13:56:51 +0800

    Finish the 4th task

--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -59,17 +59,15 @@ function copyData(barcodeinfo,iteminfo)
 function calculatePromotion(inputs) {
   var result = [];
   var promotions = loadPromotions();
-  var flag;
   for(var i=0; i<inputs.length; i++)
   {
-    flag = 0;
+    var flag = 0;
     for(var j=0; j<promotions.length;j++)
       for(var k=0; k<promotions[j].barcodes.length; k++)
         if(inputs[i].item.barcode === promotions[j].barcodes[k])
         {
           result.push(calculateTotalPrice(inputs[i]));
-          flag = 1;
-          break;
+          flag = 1;break;
         }
     if(!flag)
       result.push({cartitem:inputs[i],total:inputs[i].item.price*inputs[i].number,save:0.00})
@@ -80,4 +78,24 @@ function calculatePromotion(inputs) {
 function calculateTotalPrice(input) {
   var promote = parseInt(input.number/3);
   return {cartitem:input,total:input.item.price*(input.number-promote),save:input.item.price*promote};
+}
+
+function getReceipt(inputs) {
+  var receipt = "***<没钱赚商店>收据***\n";
+  var totalprice = 0, totalsave = 0;
+  for(var i=0; i<inputs.length; i++)
+  {
+    receipt += "名称："+inputs[i].cartitem.item.name
+            +"，数量："+inputs[i].cartitem.number
+            +inputs[i].cartitem.item.unit+"，单价："
+            +inputs[i].cartitem.item.price.toFixed(2)+"(元)，小计："
+            +inputs[i].total.toFixed(2)+"(元)\n";
+    totalprice += inputs[i].total;
+    totalsave  += inputs[i].save;
+  }
+  receipt += "----------------------\n";
+  receipt += "总计："+totalprice.toFixed(2)+"(元)\n";
+  receipt += "节省："+totalsave.toFixed(2)+"(元)\n";
+  receipt += "**********************";
+  console.log(receipt);
 }
--- pos_v1/test/task-test.js
+++ pos_v1/test/task-test.js
@@ -91,3 +91,41 @@ describe('calculatePromotion', function() {
     expect(result).toEqual(expectresult);
   });
 });
+
+describe('getReceipt', function() {
+  var inputs = [
+                  {cartitem:{item: {
+                  barcode: 'ITEM000001',
+                  name: '雪碧',
+                  unit: '瓶',
+                  price: 3.00
+                  },number:5},
+                  total:12.00,save:3.00},
+                  {cartitem:{item: {
+                    barcode: 'ITEM000003',
+                    name: '荔枝',
+                    unit: '斤',
+                    price: 15.00
+                  },number:2},total:30.00,save:0.00},
+                  {cartitem:{item: {
+                    barcode: 'ITEM000005',
+                    name: '方便面',
+                    unit: '袋',
+                    price: 4.50
+                  },number:3},total:9.00,save:4.50}];
+
+  it('Should return correct itemreceipt', function() {
+
+    spyOn(console, 'log');
+    getReceipt(inputs)
+    var expectresult =    '***<没钱赚商店>收据***\n' +
+                          '名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)\n' +
+                          '名称：荔枝，数量：2斤，单价：15.00(元)，小计：30.00(元)\n' +
+                          '名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)\n' +
+                          '----------------------\n' +
+                          '总计：51.00(元)\n' +
+                          '节省：7.50(元)\n' +
+                          '**********************';
+    expect(console.log).toHaveBeenCalledWith(expectresult);
+  });
+});
